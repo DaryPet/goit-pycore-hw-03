caa6768043e1cb47dfd5cccce7171505a176175b
DaryPet <d7akkord@gmail.com>
2024-07-02 09:34:57 +0200

    01

--- task01.py
+++ task01.py
@@ -3,17 +3,16 @@ from datetime import datetime
 
 def get_days_from_today(date):
     try:
-        # Перетворюємо рядок дати у об'єкт datetime
+       
         date_obj = datetime.strptime(date, '%Y-%m-%d').date()
         
-        # Отримуємо поточну дату
+      
         current_date = datetime.today().date()
         
-        # Розраховуємо різницю у днях
+      
         delta = current_date - date_obj
         
-        # Повертаємо різницю у днях як ціле число
         return delta.days
     except ValueError:
-        # Якщо виникає помилка (наприклад, неправильний формат дати), повертаємо None
+     
         return None
