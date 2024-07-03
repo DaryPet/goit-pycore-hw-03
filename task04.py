25cca5f793b8e56069d790787bb85995cc7b491b
DaryPet <d7akkord@gmail.com>
2024-07-03 18:10:13 +0200

    task 4

--- task04.py
+++ task04.py
@@ -14,13 +14,26 @@ def get_upcoming_birthdays(users):
         print(birthday_this_year)
 
         if birthday_this_year < today:
-            birthday_this_year = birthday_date.replace(yea = today.year+1)
+            birthday_this_year = birthday_date.replace(year = today.year+1)
+
+        if 0 <= (birthday_this_year - today).days <= 7:
+            if birthday_this_year.weekday() ==5:
+                days_to_monday = 2
+                
+                birthday_this_year + timedelta(days=days_to_monday)
+            elif birthday_this_year.weekday() == 6:
+                days_to_monday =1
+                birthday_this_year + timedelta(days=days_to_monday)
+                if (birthday_this_year - today).days > 7:
+                    birthday_this_year += timedelta(days=7) 
+        
+            upcoming_birthdays.append({"name": user["name"],
+                                       "congrats_day": birthday_this_year.strftime("%Y.%m.%d")
+                                       })
 
         
 
-        
-
-    return birthday_date
+    return upcoming_birthdays
 
 
 
@@ -28,8 +41,10 @@ def get_upcoming_birthdays(users):
 users = [
     {"name": "John Doe", "birthday": "1985.01.23"},
     {"name": "Jane Smith", "birthday": "1990.01.27"},
-    {"name": "Ann Miki", "birthday": "1990.07.08"}
+    {"name": "Ann Miki", "birthday": "1990.07.08"},
+    {"name": "An Mi", "birthday": "1990.07.05"}
 ]
 
 upcoming_birthdays = get_upcoming_birthdays(users)
 print("Список привітань на цьому тижні:", upcoming_birthdays)
+
